6513e2910d322827cd18bdf4ef44b6a9e06c11de
EdithSerrano <edith.serrano@alu.inspt.utn.edu.ar>
2024-04-17 22:51:28 -0300

    Multiplo de 3

--- Sumador.c
+++ Sumador.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 #define p printf
 #define s scanf
-
-int main(void)
+int main()
 {
-
-  int num1,num2,num3,num4,suma;
-  p("ingrese primer valor: "); 
-  s("%d", &num1);
-  p(" iNgrese segundo valor:");
-  s("%d", &num2);
-  p(" iNgrese tercer valor:");
-  s("%d", &num3);
-   p(" iNgrese cuarto valor:");
-  s("%d", &num4);
-
-
-
-  suma= num1+num2+num3+num4;
-
-    p("La suma es : %d",suma);
-
+    int num,multiplo;
+    p("Ingrese un valor:");
+    s("%d",&num);
+    
+    if((num %3)==0)
+       p("%d El numero ingresado es multiplo de 3:",multiplo);
+    else
+       p("%d El numero ingresado no es multiplo de 3:",multiplo);
+    
     return 0;
 }
