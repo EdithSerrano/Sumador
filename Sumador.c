ecad8fa1f130c9ae2f2c4ae617fdf8b9661c9f3c
EdithSerrano <edith.serrano@alu.inspt.utn.edu.ar>
2024-04-17 21:02:23 -0300

    Version v1.4

--- Sumador.c
+++ Sumador.c
@@ -5,16 +5,19 @@
 int main(void)
 {
 
-  int num1,num2,num3,suma;
+  int num1,num2,num3,num4,suma;
   p("ingrese primer valor: "); 
   s("%d", &num1);
   p(" iNgrese segundo valor:");
   s("%d", &num2);
   p(" iNgrese tercer valor:");
   s("%d", &num3);
+   p(" iNgrese cuarto valor:");
+  s("%d", &num4);
 
 
-  suma= num1+num2+num3;
+
+  suma= num1+num2+num3+num4;
 
     p("La suma es : %d",suma);
 
